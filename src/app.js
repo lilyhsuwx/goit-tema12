5129359ca778f0465561c3118862e782d09ff4e8
bwhtsu <liliiyazh5@gmail.com>
2026-05-08 13:28:06 +0300

    home work

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const targetDate = new Date('Mar 12, 2027')
+
+const daysEl = document.querySelector('[data-value="days"]');
+const hoursEl = document.querySelector('[data-value="hours"]');
+const minutesEl = document.querySelector('[data-value="mins"]');
+const secondsEl = document.querySelector('[data-value="secs"]');
+
+const day = ["неділя","понеділок","вівторок", "середа","четвер","п'ятниця","субота"]
+
+const month = ["Січень","Лютий","Березень", "Квітень","Травень","Червень","Липень","Серпень", "Вересень","Жовтень","Листопад","Грудень"]
+
+setInterval(() => {
+    const currentDate = new Date();
+    const time = (targetDate - currentDate);
+    const days = Math.floor(time / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+    const secs = Math.floor((time % (1000 * 60)) / 1000);
+
+    daysEl.textContent = days;
+    hoursEl.textContent = hours;
+    minutesEl.textContent = mins;
+    secondsEl.textContent = secs;
+}, 1000);
